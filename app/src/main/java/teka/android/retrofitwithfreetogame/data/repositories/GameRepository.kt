8999fb5a6ee2f44f0b20d7e8f9ea6f092063b4bf
samAricha <samaricha001.com>
2023-05-09 10:38:07 +0300

    getting game by id in repository done

--- app/src/main/java/teka/android/retrofitwithfreetogame/data/repositories/GameRepository.kt
+++ app/src/main/java/teka/android/retrofitwithfreetogame/data/repositories/GameRepository.kt
@@ -2,7 +2,9 @@ package teka.android.retrofitwithfreetogame.data.repositories
 
 import teka.android.retrofitwithfreetogame.data.remote.GameService
 import teka.android.retrofitwithfreetogame.domain.items.GameItem
+import teka.android.retrofitwithfreetogame.domain.items.SpecificGameItem
 import teka.android.retrofitwithfreetogame.domain.items.toGameItem
+import teka.android.retrofitwithfreetogame.domain.items.toSpecificGameItem
 import javax.inject.Inject
 
 class GameRepository @Inject constructor(private val gameService: GameService) {
@@ -12,6 +14,12 @@ class GameRepository @Inject constructor(private val gameService: GameService) {
         return gameService.getGames().map {
             it.toGameItem()
         }
+    }
+
+    suspend fun getGameById(id: Int): SpecificGameItem{
+
+        return gameService.getGameById(id).toSpecificGameItem()
+
     }
 
 }
