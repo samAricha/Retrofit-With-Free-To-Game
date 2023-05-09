8aac53ee34ebc61e61b6bf18b99f72050f3ad840
samAricha <samaricha001.com>
2023-05-09 11:20:52 +0300

    GameViewModel.kt done

---
+++ app/src/main/java/teka/android/retrofitwithfreetogame/presentation/vm/GameViewModel.kt
@@ -0,0 +1,61 @@
+package teka.android.retrofitwithfreetogame.presentation.vm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import teka.android.retrofitwithfreetogame.domain.GetGameByIdUseCase
+import teka.android.retrofitwithfreetogame.domain.GetGamesUseCase
+import teka.android.retrofitwithfreetogame.domain.items.GameItem
+import teka.android.retrofitwithfreetogame.domain.items.SpecificGameItem
+import javax.inject.Inject
+
+@HiltViewModel
+class GameViewModel @Inject constructor(
+
+    private val getGamesUseCase: GetGamesUseCase,
+    private val getGameByIdUseCase: GetGameByIdUseCase
+
+    ): ViewModel() {
+
+    private val _games = MutableLiveData<List<GameItem>>()
+    val games : LiveData<List<GameItem>> get() = _games
+
+    private val _game = MutableLiveData<SpecificGameItem>()
+    val game: LiveData<SpecificGameItem> get() =  _game
+
+    init {
+        getGames()
+    }
+
+    private fun getGames() {
+
+        viewModelScope.launch {
+            try {
+                val games = getGamesUseCase()
+                _games.value = games
+
+            }catch (_:Exception){}
+        }
+
+    }
+
+    private fun getGameById(id: Int) {
+
+        viewModelScope.launch {
+
+            try {
+
+                val game = getGameByIdUseCase(id)
+                _game.value = game
+
+            }catch (_:Exception){}
+
+        }
+
+    }
+
+
+}
